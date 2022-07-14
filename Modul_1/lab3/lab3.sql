e13cd64690bf3ece7d22f74d60f1da72397693cc
Andrey <alfa_work_mail@mail.ru>
2022-07-14 17:22:02 +0300

    lab3 all done

--- Modul_1/lab3/lab3.sql
+++ Modul_1/lab3/lab3.sql
@@ -247,3 +247,67 @@ drop table dept;
 drop cluster emp_dept_cluster; 
 
 /*Task_5*/
+-- Step 1
+CREATE cluster emp_dept_cluster( deptno NUMBER( 2 ) ) 
+    SIZE 1024  
+    STORAGE( INITIAL 100K NEXT 50K )
+    HASH IS deptno 
+    HASHKEYS 150;
+    
+-- Step 2
+-- Невозможно создать индекс в хэш-кластере
+--CREATE INDEX idxcl_emp_dept on cluster emp_dept_cluster;
+
+-- Step 3
+CREATE TABLE dept 
+  ( 
+    deptno NUMBER( 2 ) PRIMARY KEY 
+  , dname  VARCHAR2( 14 ) 
+  , loc    VARCHAR2( 13 ) 
+  ) 
+  cluster emp_dept_cluster ( deptno ) ; 
+  
+ CREATE TABLE emp 
+  ( 
+    empno NUMBER PRIMARY KEY 
+  , ename VARCHAR2( 10 ) 
+  , job   VARCHAR2( 9 ) 
+  , mgr   NUMBER 
+  , hiredate DATE 
+  , sal    NUMBER 
+  , comm   NUMBER 
+  , deptno NUMBER( 2 ) REFERENCES dept( deptno ) 
+  ) 
+  cluster emp_dept_cluster ( deptno ) ;
+
+-- Step 4
+INSERT INTO dept( deptno , dname , loc) 
+SELECT deptno , dname , loc 
+   FROM scott.dept; 
+    
+commit; 
+ 
+ INSERT INTO emp ( empno, ename, job, mgr, hiredate, sal, comm, deptno ) 
+ SELECT rownum, ename, job, mgr, hiredate, sal, comm, deptno 
+   FROM scott.emp 
+       
+commit;
+
+-- Step 5
+SELECT * 
+   FROM 
+  ( 
+     SELECT dept_blk, emp_blk, CASE WHEN dept_blk <> emp_blk THEN '*' END flag, deptno 
+       FROM 
+      ( 
+         SELECT dbms_rowid.rowid_block_number( dept.rowid ) dept_blk, dbms_rowid.rowid_block_number( emp.rowid ) emp_blk, dept.deptno 
+           FROM emp , dept 
+          WHERE emp.deptno = dept.deptno 
+      ) 
+  ) 
+ORDER BY deptno
+
+-- Step 6
+drop table emp;
+drop table dept;
+drop cluster emp_dept_cluster; 
