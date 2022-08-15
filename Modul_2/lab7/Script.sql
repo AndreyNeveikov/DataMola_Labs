9db5d370365a48dd6555eb6c7861c32ffacc4d50
Andrey <alfa_work_mail@mail.ru>
2022-08-15 19:22:41 +0300

    lab7 task_1 done

--- Modul_2/lab7/Script.sql
+++ Modul_2/lab7/Script.sql
@@ -5,16 +5,30 @@ alter session set current_schema=DW_DATA;
 CREATE MATERIALIZED VIEW mv_daily_profit
 BUILD DEFERRED
 REFRESH COMPLETE ON DEMAND
-AS SELECT /*+ gather_plan_statistics*/ order_date,
-    DECODE (GROUPING(product_name), 1, 'All products', product_name) products,
-    DECODE (GROUPING(order_status), 1, 'All statusies', order_status) statusies,
-    SUM(order_price) AS profit
-FROM dw_data.t_dw_fct_orders
-WHERE order_date < TO_DATE( '05.01.20', 'MM/DD/YY' )
-GROUP BY order_date, CUBE(product_name, order_status)
-ORDER BY 1;
+AS SELECT product_name, client_address, order_date,  sum(order_price) profit,
+
+   GROUPING(product_name) name_,
+   GROUPING(client_address) address,
+   GROUPING(order_date) date_,
+   
+   GROUPING_ID(product_name, client_address) name_address,
+   GROUPING_ID(client_address, product_name) address_name,
+   GROUPING_ID(product_name, order_date) name_date,
+   GROUPING_ID(client_address, order_date) address_date,
+   
+   GROUPING_ID(client_address, product_name, order_date) address_name_date,
+   GROUPING_ID(client_address, order_date, product_name) address_date_name,
+   GROUPING_ID(order_date, product_name, client_address) date_name_address
+   
+   FROM DW_DATA.t_dw_fct_orders fct_ord
+   LEFT JOIN t_dw_customers  cust ON cust.client_id = fct_ord.client_id
+   WHERE date_id  >= TO_DATE( '03.01.20', 'MM/DD/YY' ) AND date_id  < TO_DATE( '04.01.20', 'MM/DD/YY' )
+   GROUP BY CUBE(product_name, client_address, order_date);
+
 
 EXECUTE DBMS_MVIEW.REFRESH('mv_daily_profit');
 
 SELECT * FROM  mv_daily_profit;
+
+commit;
 
