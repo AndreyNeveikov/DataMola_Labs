7a8a17f244aa829c9bda89cb94f016102178bf76
Andrey <alfa_work_mail@mail.ru>
2022-08-16 16:18:16 +0300

    Clean lab4 dw-layer folders

--- Modul_2/lab4/DW/packages/t_dw_orders/pkg_etl_cl_orders_def.sql
+++
@@ -1,8 +0,0 @@
-alter session set current_schema=DW_CL;
-
-CREATE OR REPLACE PACKAGE pkg_etl_cl_orders
-AS  
-   PROCEDURE LOAD_CLEAN_ORDERS;
-END pkg_etl_cl_orders;
-
---drop package pkg_etl_cl_orders;
